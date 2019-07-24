55a863900a6d6483648bba8944f87f0f2368bed0
shubhi <shubhijain0606@gmail.com>
2019-07-24 14:29:18 +0530

    submitted task 4(swagger2 documentation)

---
+++ src/main/java/com/stackroute/muzix/Config/WebConfiguration.java
@@ -0,0 +1,37 @@
+package com.stackroute.muzix.Config;
+
+import org.apache.catalina.servlets.WebdavServlet;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class WebConfiguration {
+
+    // bean for h2Servlet registration
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+    ServletRegistrationBean registrationBean=new ServletRegistrationBean(new WebServlet());
+    registrationBean.addUrlMappings("/console/*");
+    return registrationBean;
+}
+
+    // bean for swagger2 documentation
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.stackroute.controller"))
+                .paths(regex("/track.*"))
+                .build();
+    }
+
+}
