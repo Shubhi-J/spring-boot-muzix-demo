0c026c9537d0413c73a9124cae86274aeaac6ec5
shubhi <shubhijain0606@gmail.com>
2019-07-24 14:25:04 +0530

    submitted task 4(swagger2 documentation)

--- src/main/java/com/stackroute/muzix/repository/TrackRepository.java
+++ src/main/java/com/stackroute/muzix/repository/TrackRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 // repository for tracks
 @Repository
 public interface TrackRepository extends JpaRepository<Track,Integer> {
+    // query to search a track by name
     @Query("FROM Track t WHERE t.name = ?1")
     Track searchTrackByName(String name);
 }
