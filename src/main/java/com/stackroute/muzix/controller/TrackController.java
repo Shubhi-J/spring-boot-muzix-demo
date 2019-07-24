8ad05e0d9ccf5b605d8d5ff28780095c1a68bdea
shubhi <shubhijain0606@gmail.com>
2019-07-24 17:50:34 +0530

    submitted task 7(global exception handling)

--- src/main/java/com/stackroute/muzix/controller/TrackController.java
+++ src/main/java/com/stackroute/muzix/controller/TrackController.java
@@ -1,6 +1,7 @@
 package com.stackroute.muzix.controller;
 
 import com.stackroute.muzix.domain.Track;
+import com.stackroute.muzix.exception.GlobalExceptionHandler;
 import com.stackroute.muzix.exception.TrackAlreadyExistException;
 import com.stackroute.muzix.exception.TrackNotFoundExeption;
 import com.stackroute.muzix.service.TrackService;
@@ -14,7 +15,7 @@ import java.util.List;
 @RestController
 // class level request mapping
 @RequestMapping("api/v1")
-public class TrackController {
+public class TrackController extends GlobalExceptionHandler {
 
     // create object of TrackService
     TrackService trackService;
@@ -27,15 +28,15 @@ public class TrackController {
 
     // method to save data with endpoint track
     @PostMapping("/track")
-    public ResponseEntity<?> saveTrack(@RequestBody Track track){
+    public ResponseEntity<?> saveTrack(@RequestBody Track track) throws TrackAlreadyExistException{
         ResponseEntity responseEntity;
-      try{
+    //  try{
           // save track
           trackService.saveTrack(track);
           responseEntity=new ResponseEntity<String>("Successfully saved", HttpStatus.CREATED);
-      } catch (TrackAlreadyExistException ex){
-          responseEntity=new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
-      }
+      //} catch (TrackAlreadyExistException ex){
+       //   responseEntity=new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
+     // }
         // return result
         return responseEntity;
     }
@@ -49,15 +50,15 @@ public class TrackController {
 
     // method to get a specific track with endpoint track/{id}
     @GetMapping("/track/{id}")
-    public ResponseEntity<?> getTrackById(@PathVariable int id) {
+    public ResponseEntity<?> getTrackById(@PathVariable int id) throws TrackNotFoundExeption {
         ResponseEntity responseEntity;
         // get track by id
-        try{
+     //   try{
             responseEntity= new ResponseEntity<Track>(trackService.getTrackById(id),HttpStatus.OK);
             // catch track not found exception
-    } catch (TrackNotFoundExeption ex){
-            responseEntity=new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
-        }
+    //} catch (TrackNotFoundExeption ex){
+      //      responseEntity=new ResponseEntity<String>(ex.getMessage(), HttpStatus.CONFLICT);
+        //}
         return responseEntity;
     }
 
@@ -71,13 +72,13 @@ public class TrackController {
     @PutMapping("/track")
     public ResponseEntity<?> updateTrack(@RequestBody Track track){
         ResponseEntity responseEntity;
-        try{
+      //  try{
             // update track
             trackService.updateTrack(track);
             responseEntity=new ResponseEntity<String>("Successfully updated", HttpStatus.OK);
-        } catch (Exception e){
-            responseEntity=new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
-        }
+       // } catch (Exception e){
+         //   responseEntity=new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
+       // }
         // return success message
         return responseEntity;
     }
@@ -85,7 +86,9 @@ public class TrackController {
     // method to get a specific track with endpoint trackByName/{name}
     @GetMapping("/trackByName/{name}")
     public ResponseEntity<?> searchTrackByName(@PathVariable String name) {
+        ResponseEntity responseEntity;
         // get track by name
-        return new ResponseEntity<Track>(trackService.searchTrackByName(name),HttpStatus.OK);
+        responseEntity= new ResponseEntity<Track>(trackService.searchTrackByName(name),HttpStatus.OK);
+    return responseEntity;
     }
 }
