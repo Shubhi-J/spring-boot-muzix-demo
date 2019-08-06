c3b3ee252dcd261d1b7b8090ea43f7612630852a
Shubhi-J <45873056+Shubhi-J@users.noreply.github.com>
2019-08-06 17:48:25 +0530

    used encapsulation

--- src/main/java/com/stackroute/muzix/controller/TrackController.java
+++ src/main/java/com/stackroute/muzix/controller/TrackController.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class TrackController {
 
     // create object of TrackService
-    TrackService trackService;
+    private TrackService trackService;
 
     // parameterised constructor
     @Autowired
